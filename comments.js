3edbde392ebfc882d613b74b29631d28acd0027f
Craig McLellen <162937021+cmclellen-insight@users.noreply.github.com>
2024-10-30 10:06:41 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,18 +1,24 @@
-// create web server
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+// Create a web server that listens for requests on the root URL and responds with a list of comments in JSON format.
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
-http.createServer(function (req, res) {
-    var q = url.parse(req.url, true);
-    var filename = "." + q.pathname;
-    fs.readFile(filename, function(err, data) {
-      if (err) {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        return res.end("404 Not Found");
-      }  
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      return res.end();
-    });
-  }).listen(8080);
+const comments = [
+  { username: 'Tammy', comment: 'lol first comment' },
+  { username: 'Jimmy', comment: 'nice one!' },
+  { username: 'Sandy', comment: 'I love it!' }
+];
+
+// Respond with the json for the comments
+app.get('/', (req, res) => {
+  res.json(comments);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+// Run the server using node comments.js and visit http://localhost:3000 to see the comments. 
+// The comments should be displayed as a JSON array of objects. 
+
+// 3.2
